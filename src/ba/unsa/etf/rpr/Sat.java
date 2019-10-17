45d2de11adfc3015823c0a6ee49087b2a60f683b
demon034 <sspahic2@etf.unsa.ba>
2019-10-17 22:27:23 +0200

    Third commit Stavio sam da main metoda sada pripada svojoj posebnoj Main klasi

--- src/ba/unsa/etf/rpr/Sat.java
+++ src/ba/unsa/etf/rpr/Sat.java
@@ -1,15 +1,15 @@
 package ba.unsa.etf.rpr;
 
-import javax.swing.*;
+//import javax.swing.*;
 
-public class Sat {
+class Sat {
     private int sati, minute, sekunde;
 
     Sat(int sati, int minute, int sekunde) {
         postavi(sati, minute, sekunde);
     }
 
-    private void postavi(int sati, int minute, int sekunde) {
+    void postavi(int sati, int minute, int sekunde) {
         this.sati = sati;
         this.minute = minute;
         this.sekunde = sekunde;
@@ -17,48 +17,50 @@ public class Sat {
 
     void sljedeci() {
         this.sekunde++;
-        if(sekunde == 60) {
+        if (sekunde == 60) {
             sekunde = 0;
             minute++;
         }
-        if(minute == 60) {
+        if (minute == 60) {
             minute = 0;
             sati++;
         }
-        if(sati == 24) {
+        if (sati == 24) {
             sati = 0;
         }
     }
 
     void prethodni() {
         sekunde--;
-        if(sekunde == -1) {
+        if (sekunde == -1) {
             sekunde = 59;
-            minute --;
+            minute--;
         }
-        if(minute == -1) {
+        if (minute == -1) {
             minute = 59;
             sati--;
         }
-        if(sati == -1)
+        if (sati == -1)
             sati = 23;
     }
 
-    public void pomjeriZa(int pomjeraj) {
-        if(pomjeraj > 0)
-            for(int i = 0; i < pomjeraj; i++)
+    void pomjeriZa(int pomjeraj) {
+        if (pomjeraj > 0)
+            for (int i = 0; i < pomjeraj; i++)
                 this.sljedeci();
         else
-            for(int i = 0; i < -pomjeraj; i++)
+            for (int i = 0; i < -pomjeraj; i++)
                 prethodni();
     }
 
-     int dajSate() {
+    int dajSate() {
         return sati;
     }
+
     int dajMinute() {
         return this.minute;
     }
+
     int dajSekunde() {
         return sekunde;
     }
@@ -66,15 +68,6 @@ public class Sat {
     void ispisi() {
         System.out.println(dajSate() + ":" + dajMinute() + ":" + dajSekunde());
     }
-    public static void main(String[] args) {
-        Sat s = new Sat(15,30,45);
-        s.ispisi();
-        s.sljedeci();
-        s.ispisi();
-        s.pomjeriZa(-48);
-        s.ispisi();
-        s.postavi(0,0,0);
-        s.ispisi();
 
-    }
 }
+
